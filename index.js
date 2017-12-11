3a7ca437fcb2f56fbd0a8c32cd6146937f8d44a2
gin1231 <jineok12@gmail.com>
2017-12-11 19:16:24 +0900

    packages added

--- index.js
+++ index.js
@@ -1,3 +1,32 @@
+// @flow
+
+import React, {Component} from 'react';
+import {Provider} from 'react-redux';
+import 'es6-symbol/implement';
 import { AppRegistry } from 'react-native';
-import App from './App';
-AppRegistry.registerComponent('easi6driver', () => App);
+import store from './src/redux/store';
+import App from './src/App';
+
+type Props = {
+
+};
+
+// class Easi6ForDriver extends Component<Props> {
+//   render() {
+//     return (
+//       <Provider store={store}>
+//         <App />
+//       </Provider>
+//     );
+//   }
+// }
+
+const Easi6ForDriver = () => {
+  return (
+    <Provider store={store}>
+      <App />
+    </Provider>
+  );
+};
+
+AppRegistry.registerComponent('easi6driver', () => Easi6ForDriver);
---
+++ src/redux/reducer.js
@@ -0,0 +1,23 @@
+import {fromJS} from 'immutable';
+import {combineReducers, loop} from 'redux-loop';
+import SessionStateReducer, {RESET_STATE} from '../modules/session/SessionState';
+// ## Generator Reducer Imports
+
+const namespacedReducer = combineReducers({
+  // Counter sample app state. This can be removed in a live application
+  // rentals: RentalStateReducer,
+  // ## Generator Reducers
+
+  // Navigator states
+
+  session: SessionStateReducer
+});
+
+export default function mainReducer(state, action) {
+  const [nextState, effects] = action.type === RESET_STATE
+    ? namespacedReducer(action.payload, action)
+    : namespacedReducer(state || void 0, action);
+
+  // enforce the state is immutable
+  return loop(fromJS(nextState), effects);
+}
