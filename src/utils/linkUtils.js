5ea9fa60a1246866bd6256b014618fce9a47ba15
gin1231 <jineok12@gmail.com>
2017-12-30 11:35:28 +0900

    link ios test 필요

--- src/utils/linkUtils.js
+++ src/utils/linkUtils.js
@@ -1,10 +1,21 @@
-import {Platform} from 'react-native';
+import {Platform, Linking} from 'react-native';
 import SendIntentAndroid from 'react-native-send-intent';
+
+const openUrlIos = (url) => {
+  Linking.canOpenURL(url).then(supported => {
+    if (!supported) {
+      console.log('Can\'t handle url: ' + url);
+    } else {
+      return Linking.openURL(url);
+    }
+  }).catch(err => console.error('An error occurred', err));
+};
 
 export const openMapApp = (location) => {
   const isIos = Platform.OS === 'ios';
   if (isIos) {
-
+    const url = `http://maps.apple.com/?ll=${location.latitude},${location.longitude}`;
+    openUrlIos(url);
   } else {
     SendIntentAndroid.openMaps(`${location.latitude},${location.longitude}`)
   }
@@ -17,7 +28,8 @@ const isValidPhone = (phone) => {
 export const openPhoneApp = (phone) => {
   const isIos = Platform.OS === 'ios';
   if (isIos) {
-
+    const url = `tel:${phone}`;
+    openUrlIos(url);
   } else {
     if (phone && isValidPhone(phone))
     SendIntentAndroid.sendPhoneDial(phone);
