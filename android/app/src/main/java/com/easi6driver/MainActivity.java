895fd854676806d387bb37086cf0ae1323367f1f
gin1231 <jineok12@gmail.com>
2018-01-09 17:06:02 +0900

    android splash activity 구현

--- android/app/src/main/java/com/easi6driver/MainActivity.java
+++ android/app/src/main/java/com/easi6driver/MainActivity.java
@@ -1,7 +1,15 @@
 package com.easi6driver;
 
+import android.support.annotation.LayoutRes;
+
 import com.reactnativenavigation.controllers.SplashActivity;
 
 public class MainActivity extends SplashActivity {
+
+    @LayoutRes
+    @Override
+    public int getSplashLayout() {
+        return R.layout.landing_activity;
+    }
 
 }
