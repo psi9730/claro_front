fb77a05439d1af7fb21593f05a0ca6088e598d65
psi9730 <psi9730@naver.com>
2018-08-22 17:36:03 +0900

    fix error

---
+++ ios/ClaroWidget/TodayViewController.h
@@ -0,0 +1,13 @@
+//
+//  TodayViewController.h
+//  ClaroWidget
+//
+//  Created by easi6 mbp13 on 16/08/2018.
+//  Copyright © 2018 Facebook. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface TodayViewController : UIViewController
+
+@end
